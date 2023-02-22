acc43366f89a1a4b6cf6e410eb01becdf0c3ecc1
hyzerki <hyzerki@gmail.com>
2023-02-22 11:53:41 +0300

    lab2

--- lab2/02-05.js
+++ lab2/02-05.js
@@ -1,25 +1,11 @@
-const fs = require("fs");
 const http = require("http");
+const fs = require("fs");
 
-const res = function (request, response) {
-    response.setHeader("Content-Type", "text/plain; charset=utf-8;");
-    response.writeHead(200);
-    response.end("Мелешко Никита Сергеевич");
-}
 
-let server = http.createServer(function (request, response) {
-    if (request.url == "/jquery" && request.method == "GET") {
-        response.setHeader("Content-Type", "text/html; charset=utf-8;");
-        response.writeHead(200);
-        fs.createReadStream("jquery.html").pipe(response);
-    } else if (request.url == "/api/name" && request.method == "GET") {
-        setTimeout((request, response) => {
-            res(request, response);
-        }, 3000, request, response);
-    } else {
-        response.writeHead(404);
-        response.end();
+http.createServer((req, res)=>{
+    if(req.url === '/fetch'){
+        fs.createReadStream('./fetch.html').pipe(res);
+    }else if( req.url==='/api/name'){
+        res.end("Никита Мелешко");
     }
-});
-
-server.listen(3000);
+}).listen(3000);

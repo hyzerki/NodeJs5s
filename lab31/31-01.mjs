4f64ab1a958b883bf88b2de52b9696794b3c54f3
Meleshko Nikita <hyzerki@gmail.com>
2023-09-26 21:45:43 +0300

    lab31

--- lab31/31-01.mjs
+++ lab31/31-01.mjs
@@ -1,8 +1,8 @@
-import { Telegraf } from "telegraf";
-import { message } from "telegraf/filters";
+import TelegramBot from "node-telegram-bot-api";
+const token = "6531139546:AAFCJSGhG6qJyOMLRo8Xj62Uop8Z0jsWiiY";
 
-const bot = new Telegraf("6531139546:AAFCJSGhG6qJyOMLRo8Xj62Uop8Z0jsWiiY");
+const bot = new TelegramBot(token, {polling: true});
+bot.on("message", (msg) => {
+    bot.sendMessage(msg.chat.id, `echo: ${msg.text}`);
+});
 
-bot.on(message("text"), (ctx) => ctx.reply(`echo: ${ctx.message.text}`));
-
-bot.launch();
